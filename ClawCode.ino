62f10033f9047ac8411db9ce280dfa38e84b3e26
McSanch <57806354+McSanch@users.noreply.github.com>
2021-05-11 15:30:21 -0600

    Update ClawCode.ino

--- ClawCode.ino
+++ ClawCode.ino
@@ -2,13 +2,16 @@
 PRIZM prizm;
 
 int distance;
-int closeSize;
-int closeSpeed;
+int distance2;
+int verticalDistance;
+bool isNear;
 
 void setup() {
   prizm.PrizmBegin();
   prizm.setServoSpeed(1,100);
-  distance = 50;
+  prizm.setServoSpeed(2,15);
+  distance = 0;
+  Serial.begin(9600);
 }
 
 void openClaws() {
@@ -16,12 +19,70 @@ void openClaws() {
 }
 
 void closeClaws() {
-  prizm.setServoPosition(1,115);
+  prizm.setServoPosition(1,100);
+}
+
+void liftArm() {
+  prizm.setServoPosition(2,50); 
+}
+
+void lowerArm() {
+  prizm.setServoPosition(2,100); 
+}
+
+void setMovement(int y, int x) {
+  prizm.setCRServoState(y, x);
+}
+
+void dropOffPart() {
+  isNear = false;
+  distance2 = prizm.readSonicSensorCM(2);
+  setMovement(1, -100);
+  setMovement(2, 100);
+  while (isNear == false) {if (distance2 < 2) {isNear = true;} else {distance2 = prizm.readSonicSensorCM(2);}}
+  setMovement(1,0);
+  setMovement(2,0);
+  lowerArm();
+  delay(1000);
+  openClaws();
+}
+
+void returnToStart() {
+  isNear = false;
+  liftArm();
+  closeClaws();
+  distance = prizm.readSonicSensorCM(4);
+  Serial.print("TurnOn");
+  setMovement(1, 100);
+  setMovement(2, -100);
+  while (isNear == false) {
+    if (distance < 3) {
+      isNear = true;
+    }else{
+      distance = prizm.readSonicSensorCM(4);
+    }
+  }
+  setMovement(1,0);
+  setMovement(2,0);
 }
 
 void loop() {
-  distance = prizm.readSonicSensorCM(2);
-  closeSize = map(distance, 0, 25, 70, 180);
-  closeSpeed = map(distance, 0, 25, 0, 100);
-  if (distance < 26) {prizm.setServoPosition(1, closeSize); prizm.setServoSpeed(1,closeSpeed);};
+  closeClaws();
+  liftArm();
+  delay(2000);
+  openClaws();
+  delay(100);
+  lowerArm();
+  delay(1000);
+  closeClaws();
+  delay(500);
+  liftArm();
+  dropOffPart();
+  delay(1000);
+  returnToStart();
+//  if (prizm.readSonicSensorCM(3) < 3) {
+//    liftArm();
+//    closeClaws();
+//    dropOffPart();
+//  }
 }
